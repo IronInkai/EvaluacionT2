c07cda88aa2a0b6e69854b73626f4fec391710dd
SUITE <SUITE@SUITESB605-ST03>
2025-10-14 21:39:06 -0500

    Proyecto iniciado

---
+++ src/test/java/com/evaluaciont1/spring_jpa_crud/SpringJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluaciont1.spring_jpa_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
